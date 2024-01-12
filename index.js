613a0b4dac53a31631c73c18339954d048ca7e57
JrSchoolAccount <jesperx2000@gmail.com>
2024-01-12 10:53:18 +0100

    renamed .html files

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/', async (request, response) => {
   response.send(html);
 });
 
-app.use('/static', express.static('./static'));
+app.get('/:page', async (request, response) => {
+  const buf = await fs.readFile(request.params.page);
+  const html = buf.toString();
+
+  response.send(html);
+});
+
+app.use(
+  '/static',
+  express.static('./static', {
+    extensions: ['html', 'htm'],
+  })
+);
 
 app.listen(3080);
