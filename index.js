7dcdf1a0a17d31df7eca08eafd508c1728331fe3
JrSchoolAccount <jesperx2000@gmail.com>
2024-01-16 09:41:31 +0100

    major overhaul

--- index.js
+++ index.js
@@ -7,21 +7,16 @@ app.get('/', async (req, res) => {
   const buf = await fs.readFile('./static/index.html');
   const html = buf.toString();
 
-  res.render(html);
+  res.send(html);
 });
 
 app.get('/:page', async (req, res) => {
-  const buf = await fs.readFile(req.params.page);
+  const buf = await fs.readFile('./static/' + req.params.page + '.html');
   const html = buf.toString();
 
-  res.render(html);
+  res.send(html);
 });
 
-app.use(
-  '/static',
-  express.static('./static', {
-    extensions: ['html', 'htm'],
-  })
-);
+app.use('/static', express.static('./static'));
 
 app.listen(3080);
