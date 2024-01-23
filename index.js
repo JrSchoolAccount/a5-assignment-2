0f53d30c0a991c022e50769c0b99cf9d017f3479
JrSchoolAccount <jesperx2000@gmail.com>
2024-01-23 13:59:47 +0100

    Added page for movies and render them from api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs/promises';
 import ejs from 'ejs';
+import { loadMovie, loadMovies } from './static/movies.js';
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -23,7 +24,23 @@ app.get('/evenemang', async (req, res) => {
 });
 
 app.get('/filmer', async (req, res) => {
-  res.render('filmer');
+  try {
+    const movies = await loadMovies();
+    res.render('filmer', { movies });
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/filmer/:movieId', async (req, res) => {
+  try {
+    const movie = await loadMovie(req.params.movieId);
+    res.render('film', { movie });
+  } catch (error) {
+    console.error(`Error fetching movie with ID ${req.params.movieId}:`, error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.use('/static', express.static('./static'));
